8bdb156f79d03cfda04c966a5a98bba2e551c8d3
rodney kahane <rjkahane@gmail.com>
2018-06-06 13:23:55 -0400

    starting java code, testing to see if button listener works

--- app/src/main/java/com/aaclogic/dicee/MainActivity.java
+++ app/src/main/java/com/aaclogic/dicee/MainActivity.java
@@ -2,6 +2,10 @@ package com.aaclogic.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Button rollButton;
+        rollButton=findViewById(R.id.rollButton);
+
+        ImageView leftDice = findViewById(R.id.image_leftDice);
+        ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Dicee","ze button has been pressed!");
+            }
+        });
+
+
+    }//end onCreate
+
+
+}// end class
 
