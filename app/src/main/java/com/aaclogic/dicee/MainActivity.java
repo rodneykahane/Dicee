f9caeb91212209c47db805b83006621f04be60c9
rodney kahane <rjkahane@gmail.com>
2018-06-06 14:07:19 -0400

    added code for random rolls and to have dice images reflect the number on the rolls

--- app/src/main/java/com/aaclogic/dicee/MainActivity.java
+++ app/src/main/java/com/aaclogic/dicee/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,16 +19,34 @@ public class MainActivity extends AppCompatActivity {
         Button rollButton;
         rollButton=findViewById(R.id.rollButton);
 
-        ImageView leftDice = findViewById(R.id.image_leftDice);
-        ImageView rightDice = findViewById(R.id.image_rightDice);
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };//end diceArray
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Log.d("Dicee","ze button has been pressed!");
-            }
-        });
+                Log.d("Dicee","ze button has been pressed!");//debugging
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                int number2 = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee","The random number is "+number);//debugging
+
+                leftDice.setImageResource(diceArray[number]);
+                rightDice.setImageResource(diceArray[number2]);
+
+            }//end onClick
+        });//end rollButton listener
 
 
     }//end onCreate
